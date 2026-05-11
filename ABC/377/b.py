e559c8577768ba5c53523eb0141c17cb40c7daab
KoseiTanno <kosei.t44193@gmail.com>
2026-05-11 12:00:00 +0900

    Day45 0:10

--- ABC/377/b.py
+++ ABC/377/b.py
@@ -1,3 +1,20 @@
 # https://atcoder.jp/contests/abc377/tasks/abc377_b
 # 私の回答
 
+N = 8
+S = []
+cnt = 0
+out_row = set()
+out_col = set()
+for i in range(N):
+    S.append(input())
+for i in range(N):
+    for j in range(N):
+        if S[i][j] == "#":
+            out_row.add(i)
+            out_col.add(j)
+for i in range(N):
+    for j in range(N):
+        if (S[i][j] == ".") and (i not in out_row) and (j not in out_col):
+            cnt += 1
+print(cnt)

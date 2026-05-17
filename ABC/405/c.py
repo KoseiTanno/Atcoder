0cb7ee94ec37f463d980084da1863060dbe1c99f
KoseiTanno <kosei.t44193@gmail.com>
2026-05-17 18:27:04 +0900

    Day28 0:51

--- ABC/405/c.py
+++ ABC/405/c.py
@@ -1,8 +1,8 @@
+# 私の回答
+# 正解
+# 一回全てを掛け合わせたものを作って、後から過剰分を引く
 N = int(input())
 A = list(map(int,input().split()))
-result = 0
-for i in range(N):
-    for j in range(N):
-        if j != i:
-            result += A[i]*A[j]
-print(result//2)
+total = sum(A)*sum(A)
+total -= sum([i*i for i in A])
+print(total//2)

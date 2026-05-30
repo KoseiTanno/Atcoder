7ace4b12f2d5c71f22cd35ce5c07ac79fce51acc
KoseiTanno <kosei.t44193@gmail.com>
2026-05-30 12:00:00 +0900

    Day39 0:25

--- ABC/386/b.py
+++ ABC/386/b.py
@@ -1,9 +1,15 @@
 S = input()
-buttons = ["0","1","2","3","4","5","6","7","8","9"]
-zz = "00"
-while S != "":
-    lst = []
-    if zz in S:
-        S.remove(zz)
-    for i in buttons:
-        if
+N = len(S)
+lst = []
+i = 0
+cnt = 0
+while "".join(lst) != S:
+    cnt += 1
+    if S[i] == "0" and i+1 <= N-1:
+        if S[i] == S[i+1] == "0":
+            lst.append("00")
+            i += 2
+            continue
+    lst.append(S[i])
+    i += 1
+print(cnt)

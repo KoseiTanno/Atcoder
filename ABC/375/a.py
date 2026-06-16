c6dd097118bc5473c17e577baec7d49746499408
KoseiTanno <kosei.t44193@gmail.com>
2026-06-16 12:00:00 +0900

    Day47 0:30

--- ABC/375/a.py
+++ ABC/375/a.py
@@ -1,3 +1,11 @@
 # https://atcoder.jp/contests/abc375/tasks/abc375_a
 # 私の回答
+# 正解
 
+N = int(input())
+S = input()
+cnt = 0
+for i in range(N-2):
+    if S[i] == "#" and S[i+2] == "#" and S[i+1] == ".":
+        cnt += 1
+print(cnt)
--- ABC/375/b.py
+++ ABC/375/b.py
@@ -1,3 +1,18 @@
 # https://atcoder.jp/contests/abc375/tasks/abc375_b
 # 私の回答
+# 正解
+import math
+
+N = int(input())
+total_cost = 0
+pos_lst = [[0,0]]
+for i in range(N):
+    X,Y = map(int,input().split())
+    pos_lst.append([X,Y])
+pos_lst.append([0,0])
+for i in range(1,N+2):
+    cost = math.sqrt((pos_lst[i-1][0]-pos_lst[i][0])**2+((pos_lst[i-1][1]-pos_lst[i][1])**2))
+    total_cost += math.sqrt((pos_lst[i-1][0]-pos_lst[i][0])**2+((pos_lst[i-1][1]-pos_lst[i][1])**2))
+    pre_X,pre_Y = X,Y
+print(total_cost)
 

faaa6f9eb35208e427d8b18f7d1fec64e15aced7
KoseiTanno <kosei.t44193@gmail.com>
2026-07-01 12:00:00 +0900

    Day56 0:46

--- ABC/365/b.py
+++ ABC/365/b.py
@@ -1,3 +1,9 @@
 # https://atcoder.jp/contests/abc365/tasks/abc365_b
 # 私の回答
+# 正解
+
+N = int(input())
+A = list(map(int,input().split()))
+A[A.index(max(A))] = 0
+print(A.index(max(A))+1)
 
--- ABC/366/b.py
+++ ABC/366/b.py
@@ -1,2 +1,15 @@
 # https://atcoder.jp/contests/abc366/tasks/abc366_b
 # 私の回答
+# 正解
+# 転置
+# 列の最大要素数にサイズを合わせるために"*"を入れるが、行の最後の要素が"*"にならないようにする
+
+N = int(input())
+S = [input() for _ in range(N)]
+M = max(len(i) for i in S)
+
+for j in range(M):
+    row = []
+    for i in range(N-1,-1,-1):
+        row.append(S[i][j] if j < len(S[i]) else "*")
+    print(''.join(row).rstrip("*"))
